3a14c9bb02bdfc3aebb9eba08c9a31d71b0b0e12
Sebastian <sgs@zedat.fu-berlin.de>
2013-03-07 10:40:16 +0100

    Revert "Merge branch 'screens' of https://github.com/swp-sebastian/zombiefu"
    This reverts commit 007c31df4942c4c7656ea0cc6168f861f977e13b, reversing
    changes made to 707089b84296255adad3656f859ba630c7e48017.

--- src/rogue/Rogue.java
+++ src/rogue/Rogue.java
@@ -4,48 +4,39 @@ import jade.core.World;
 import jade.ui.TiledTermPanel;
 import jade.util.datatype.ColoredChar;
 import java.awt.Color;
-import java.util.Collection;
-
 import rogue.creature.Monster;
 import rogue.creature.Player;
 import rogue.level.Level;
-import rogue.screen.Screen;
 
-public class Rogue {
-
-    public static void main(String[] args) throws InterruptedException {
-
-        TiledTermPanel term = TiledTermPanel.getFramedTerminal("The Final Exam - Die Anwesenheitspflicht schlägt zurück");
-
-        // Der neue Spieler
+public class Rogue
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        TiledTermPanel term = TiledTermPanel.getFramedTerminal("Jade Rogue");
+        term.registerTile("dungeon.png", 5, 59, ColoredChar.create('#'));
+        term.registerTile("dungeon.png", 3, 60, ColoredChar.create('.'));
+        term.registerTile("dungeon.png", 5, 20, ColoredChar.create('@'));
+        term.registerTile("dungeon.png", 14, 30, ColoredChar.create('D', Color.red));
+        
         Player player = new Player(term);
-        // Die neue Welt, in der sich nun der neue Spieler befindet
-        World world = new Level(70, 30, player);
-        // Der Drache kommt hinzu
-        world.addActor(new Monster(ColoredChar.create('D', Color.yellow)));
+        World world = new Level(69, 24, player);
+        world.addActor(new Monster(ColoredChar.create('D', Color.red)));
+        term.registerCamera(player, 5, 5);
+        
+        // hallo
+        
+        while(!player.expired())
+        {
+            term.clearBuffer();
+            for(int x = 0; x < world.width(); x++)
+                for(int y = 0; y < world.height(); y++)
+                    term.bufferChar(x + 11, y, world.look(x, y));
+            term.bufferCameras();
+            term.refreshScreen();
 
-        Screen.showImage(term,world,"src/rogue/screen/startscreen.txt");
-
-        while(!player.expired()) {       
-                // prüft ob der Drache mich berührt.
-                Collection<Monster> monsters = world.getActorsAt(Monster.class, player.pos());
-                if (!monsters.isEmpty()) {
-                        Screen.showImage(term, world, "src/rogue/screen/endscreen.txt");
-                        player.expire();
-                        continue;
-                }
-                // nun wird der Screen neu gebildet
-                term.clearBuffer();
-                for(int x = 0; x < world.width(); x++) {
-                    for(int y = 0; y < world.height(); y++) {
-                        term.bufferChar(x/* + 11*/, y, world.look(x, y));
-		    }
-                }
-                term.bufferCameras();
-                term.refreshScreen();
-
-                world.tick();
+            world.tick();
         }
+
         System.exit(0);
     }
 }
