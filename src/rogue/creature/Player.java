877046afcba50b15d1c313b106c4b88c2e991879
Walfischfutter <tomas.bayer@fu-berlin.de>
2013-03-07 13:40:18 +0100

    Monster können angreifen, Endscreen kommt immer beim Sterben, ASDW zur Steuerung, etc.

--- src/rogue/creature/Player.java
+++ src/rogue/creature/Player.java
@@ -5,20 +5,26 @@ import jade.fov.RayCaster;
 import jade.fov.ViewField;
 import jade.ui.Camera;
 import jade.ui.Terminal;
+import jade.ui.TiledTermPanel;
 import jade.util.datatype.ColoredChar;
 import jade.util.datatype.Coordinate;
 import jade.util.datatype.Direction;
+import rogue.screen.Screen;
 
 public class Player extends Creature implements Camera
 {
     private Terminal term;
     private ViewField fov;
     private int intelligenceValue;
+    private int money;
+    private int ects;
+    private int semester;
     private int maximalHealthPoints;
 
     public Player(Terminal term)
     {
         super(ColoredChar.create('@'));
+        this.name = "Player";
         this.term = term;
         fov = new RayCaster();
     }
@@ -36,10 +42,7 @@ public class Player extends Creature implements Camera
                     expire();
                     break;
                 case 'x':
-                    attack(x()+1,y());
-                    attack(x(),y()+1);
-                    attack(x()-1,y());
-                    attack(x(),y()-1);
+                    roundHouseKick();
                     break;
                 default:
                     Direction dir = Direction.keyToDir(key);
