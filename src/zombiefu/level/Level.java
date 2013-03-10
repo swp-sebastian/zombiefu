c59ae9d9c0575b3c7e0085a9387d7b3084de58fa
Walfischfutter <tomas.bayer@gmail.com>
2013-03-10 19:24:06 +0100

    Terminal refresh-Fix

--- src/zombiefu/level/Level.java
+++ src/zombiefu/level/Level.java
@@ -14,8 +14,7 @@ import zombiefu.creature.Zombie;
 import zombiefu.items.Teleporter;
 import zombiefu.map.RoomBuilder;
 
-public class Level extends World
-{
+public class Level extends World {
 
     public Level(int width, int height, Generator gen) {
         super(width, height);
@@ -26,27 +25,29 @@ public class Level extends World
     public Level(int width, int height, String level) {
         this(width, height, new RoomBuilder(level, "src/sources/CharSet.txt"));
     }
-    
+
     public void fillWithEnemies() {
-        Teleporter tele = new Teleporter(ColoredChar.create('\u25A0',Color.decode("0x8B4513")),"Testraum","TestraumZ",
-    			new Coordinate(33,19),new Coordinate(33,1));
-    	// Ein Teleporter kommt hinzu
-    	addActor(tele, tele.aktuell());
-    	// Der Zombie-Dozent kommt hinzu.
+        Teleporter tele = new Teleporter(ColoredChar.create('\u25A0', Color.decode("0x8B4513")), "Testraum", "TestraumZ",
+                new Coordinate(33, 19), new Coordinate(33, 1));
+        // Ein Teleporter kommt hinzu
+        addActor(tele, tele.aktuell());
+        // Der Zombie-Dozent kommt hinzu.
         addActor(new DozentZombie());
         // 6 normale Zombies kommen hinzu
-        for(int i = 0; i <= 5; i++)
-            addActor(new Zombie());        
+        for (int i = 0; i <= 5; i++) {
+            addActor(new Zombie());
+        }
     }
 
-    public void refresh(ZombiePanel term){
-    	term.clearBuffer();
-        for(int x = 0; x < width(); x++) {
-            for(int y = 0; y < height(); y++)
+    public void refresh(ZombiePanel term) {
+        term.clearBuffer();
+        for (int x = 0; x < width(); x++) {
+            for (int y = 0; y < height(); y++) {
                 term.bufferChar(x, y, look(x, y));
-            term.bufferCameras();
-            term.refreshScreen();
+            }
         }
+        term.bufferCameras();
+        term.refreshScreen();
         tick();
     }
 }
