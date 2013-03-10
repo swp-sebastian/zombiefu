79a6a7a611e7390365f966720efde09c9b2fdb58
Max Willert <willerma@zedat.fu-berlin.de>
2013-03-10 12:55:11 +0100

    Monster verfolgen Spieler
    Alle Monster (nicht nur die Zombies) verfolgen nun den Spieler.

--- src/zombiefu/creature/Zombie.java
+++ src/zombiefu/creature/Zombie.java
@@ -1,12 +1,7 @@
 package zombiefu.creature;
 
-import jade.util.Dice;
-import jade.util.Guard;
 import jade.util.datatype.ColoredChar;
-import jade.util.datatype.Coordinate;
-import jade.util.datatype.Direction;
 import java.awt.Color;
-import java.util.Arrays;
 import zombiefu.items.Waffe;
 
 public class Zombie extends Monster {
@@ -14,18 +9,5 @@ public class Zombie extends Monster {
     public Zombie() {
         super(ColoredChar.create('\u263F', Color.GREEN), "Zombie", 1, 1, 1, new Waffe("Axt", 1,ColoredChar.create('|')));
     }
-
-    @Override
-    public void act() {
-        // TODO: Player suchen
-        Player player = world().getActor(Player.class);
-        Guard.argumentIsNotNull(player);
-        Coordinate playerPos = player.pos();
-
-        double distance = playerPos.distance(pos());
-        if(distance <= 5)
-            tryToMove(pos().directionTo(playerPos));
-        else
-            tryToMove(Dice.global.choose(Arrays.asList(Direction.values())));
-    }
+    
 }
