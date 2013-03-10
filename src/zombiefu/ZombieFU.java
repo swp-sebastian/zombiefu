d9bd1f0aafd64e6cd212ab34e2a7fa7f7ac754cd
Walfischfutter <tomas.bayer@fu-berlin.de>
2013-03-10 19:51:25 +0100

    statische Methode um Level(World) aus Datei zu erzeugen.

--- src/zombiefu/ZombieFU.java
+++ src/zombiefu/ZombieFU.java
@@ -12,11 +12,12 @@ public class ZombieFU {
         // Der neue Spieler
         Player player = new Player(term);
         // Die neue Welt, in der sich nun der neue Spieler befindet
-        Screen.showImage(term,"src/sources/startscreen.txt");
-        Level world = new Level(68, 21, "src/sources/Testraum.txt");
+        Screen.showImage(term, "src/sources/startscreen.txt");
+        Level world = Level.levelFromFile("src/sources/Testraum.txt");
         world.addActor(player);
-        while(!player.expired())
-        	((Level) player.world()).refresh(term);
+        while (!player.expired()) {
+            ((Level) player.world()).refresh(term);
+        }
         Screen.showImage(term, "src/sources/endscreen.txt");
         System.exit(0);
     }
--- src/zombiefu/creature/DozentZombie.java
+++ src/zombiefu/creature/DozentZombie.java
@@ -4,10 +4,9 @@ import jade.util.datatype.ColoredChar;
 import java.awt.Color;
 import zombiefu.items.Waffe;
 
-public class DozentZombie extends Monster
-{
-    public DozentZombie()
-    {
-        super(ColoredChar.create('\u265E', Color.RED),"Prof. Jung",1,1,1,new Waffe("Feuriger Atem",100,ColoredChar.create('|')));
+public class DozentZombie extends Monster {
+
+    public DozentZombie() {
+        super(ColoredChar.create('\u265E', Color.RED), "Prof. Jung", 1, 1, 1, new Waffe("Feuriger Atem", 100, ColoredChar.create('|')));
     }
 }
