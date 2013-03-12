dd39e245e50497e4b8d54a43edaa8a42c17d7c75
Max Willert <willerma@zedat.fu-berlin.de>
2013-03-12 16:02:25 +0100

    Waffen aus Datei einlesen, Prototyp
    Waffen können aus einer Datei ausgelesen werden und werden dann in einer
    Map gespeichert.

--- src/zombiefu/items/Waffe.java
+++ src/zombiefu/items/Waffe.java
@@ -3,42 +3,58 @@ package zombiefu.items;
 import jade.util.datatype.ColoredChar;
 
 /**
- *
+ * 
  * @author tomas
  */
 public class Waffe extends Item {
 
-    private int damage;
-    private boolean ranged;
-    private boolean directed;
-    private Waffentyp wtyp;
-    private final int blastRadius = 2; // TODO: Individuell
-    private final int range = 6; // TODO: Individuell
+	private int damage;
+	private boolean ranged;
+	private boolean directed;
+	private Waffentyp wtyp;
+	private int blastRadius = 2; // TODO: Individuell
+	private int range = 6; // TODO: Individuell
 
-    public Waffe(ColoredChar c, String n, int d, Waffentyp w) {
-    	super(c, n);
-        damage = d;
-        wtyp = w;
-    }
-    
-    public Waffe(ColoredChar c, String n, int d) {
-        this(c, n, d, Waffentyp.NAHKAMPF);
-    }
-    
-    public int getDamage() {
-        return damage;
-    }
-    
-    public int getRange() {
-        return range;
-    }
-    
-    public int getBlastRadius() {
-        return blastRadius;
-    }
-    
-    public Waffentyp getTyp() {
-        return wtyp;
-    }
-    
+	public Waffe(ColoredChar c, String n, int d, Waffentyp w) {
+		super(c, n);
+		damage = d;
+		wtyp = w;
+	}
+
+	public Waffe(ColoredChar c, String n, int damage, int range,
+			int blastRadius, Waffentyp w) {
+		this(c, n, damage, w);
+		this.blastRadius = blastRadius;
+		this.range = range;
+	}
+
+	public Waffe(ColoredChar c, String n, int damage, int anderes, Waffentyp w) {
+		this(c, n, damage, w);
+		if (w.equals(Waffentyp.FERNKAMPF)) {
+			this.range = anderes;
+		} else {
+			this.blastRadius = anderes;
+		}
+	}
+
+	public Waffe(ColoredChar c, String n, int d) {
+		this(c, n, d, Waffentyp.NAHKAMPF);
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public int getBlastRadius() {
+		return blastRadius;
+	}
+
+	public Waffentyp getTyp() {
+		return wtyp;
+	}
+
 }
