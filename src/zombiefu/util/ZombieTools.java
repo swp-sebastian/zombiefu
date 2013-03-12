87094af692262dd66576e46d33db6f57934c769f
Walfischfutter <tomas.bayer@gmail.com>
2013-03-12 22:34:16 +0100

    Kleinkram.

--- src/zombiefu/util/ZombieTools.java
+++ src/zombiefu/util/ZombieTools.java
@@ -3,6 +3,11 @@ package zombiefu.util;
 import jade.util.Dice;
 import jade.util.Guard;
 import jade.util.datatype.Direction;
+import static jade.util.datatype.Direction.EAST;
+import static jade.util.datatype.Direction.NORTH;
+import static jade.util.datatype.Direction.ORIGIN;
+import static jade.util.datatype.Direction.SOUTH;
+import static jade.util.datatype.Direction.WEST;
 import java.awt.Color;
 import java.util.Arrays;
 import java.util.List;
@@ -16,6 +21,22 @@ public class ZombieTools {
                 Direction.NORTH);
     }
 
+    public static Direction getReversedDirection(Direction dir) {
+        switch (dir) {
+            case NORTH:
+                return Direction.SOUTH;
+            case SOUTH:
+                return Direction.NORTH;
+            case EAST:
+                return Direction.WEST;
+            case WEST:
+                return Direction.EAST;
+            case ORIGIN:
+                return Direction.ORIGIN;
+        }
+        return null;
+    }
+
     public static Direction getRandomDirection() {
         return Dice.global.choose(getAllowedDirections());
     }
@@ -27,21 +48,5 @@ public class ZombieTools {
 
     public static Color getRandomColor() {
         return getRandomColor(Dice.global);
-    }
-
-    public static Direction getReversedDirection(Direction dir) {
-        switch (dir) {
-        case NORTH:
-            return Direction.SOUTH;
-        case SOUTH:
-            return Direction.NORTH;
-        case EAST:
-            return Direction.WEST;
-        case WEST:
-            return Direction.EAST;
-        case ORIGIN:
-            return Direction.ORIGIN;
-        }
-        return null;
     }
 }
