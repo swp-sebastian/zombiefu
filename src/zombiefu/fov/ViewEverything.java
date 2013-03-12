e1bb81de0a566bff70b19f0e8b6a0dc30c41fa86
Walfischfutter <tomas.bayer@fu-berlin.de>
2013-03-12 01:02:11 +0100

    konstrukt um viewfield mit Taste 'F' umzuschalten.

---
+++ src/zombiefu/fov/ViewEverything.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiefu.fov;
+
+import jade.core.World;
+import jade.fov.ViewField;
+import jade.util.datatype.Coordinate;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class ViewEverything extends ViewField {
+    
+    @Override
+    protected Collection<Coordinate> calcViewField(World world, int x, int y, int r) {
+        Collection<Coordinate> output = new HashSet<>();
+        for (int i = 0; i < world.width(); i++) {
+            for (int j = 0; j < world.height(); j++) {
+                output.add(new Coordinate(i, j));
+            }
+        }
+        return output;
+    }
+}
