d762bc7baba9e7c947365ff7ecc11cae7ab9d027
Max Willert <willerma@zedat.fu-berlin.de>
2013-03-12 12:57:49 +0100

    Zufällig Zombies
    Zombies werden zufällig erzeugt, Problem: beim ständigen Wechsel kommen
    immer mehr dazu?

--- src/zombiefu/creature/Player.java
+++ src/zombiefu/creature/Player.java
@@ -21,209 +21,227 @@ import zombiefu.items.Item;
 import zombiefu.level.Level;
 import zombiefu.ui.ZombieFrame;
 import zombiefu.util.Screen;
+import zombiefu.util.TargetIsNotInThisWorldException;
 import zombiefu.util.ZombieTools;
 
 public class Player extends Creature implements Camera {
 
-    public ZombieFrame frame;
-    private ViewField fov;
-    private int intelligenceValue;
-    private int money;
-    private int ects;
-    private int semester;
-    private int maximalHealthPoints;
-    private ArrayList<ConsumableItem> inventar;
-    private ArrayList<Waffe> waffen;
+	public ZombieFrame frame;
+	private ViewField fov;
+	private int intelligenceValue;
+	private int money;
+	private int ects;
+	private int semester;
+	private int maximalHealthPoints;
+	private ArrayList<ConsumableItem> inventar;
+	private ArrayList<Waffe> waffen;
 
-    public Player(ZombieFrame frame, ColoredChar face, String name,
-            int healthPoints, int attackValue, int defenseValue,
-            int intelligenceValue, Waffe w) {
+	public Player(ZombieFrame frame, ColoredChar face, String name,
+			int healthPoints, int attackValue, int defenseValue,
+			int intelligenceValue, Waffe w) {
 
-        super(face, name, healthPoints, attackValue, defenseValue);
+		super(face, name, healthPoints, attackValue, defenseValue);
 
-        this.maximalHealthPoints = healthPoints;
-        this.intelligenceValue = intelligenceValue;
-        this.frame = frame;
-        this.godMode = true;
-        this.money = 10;
-        this.ects = 0;
-        this.semester = 1;
+		this.maximalHealthPoints = healthPoints;
+		this.intelligenceValue = intelligenceValue;
+		this.frame = frame;
+		this.godMode = true;
+		this.money = 10;
+		this.ects = 0;
+		this.semester = 1;
 
-        this.inventar = new ArrayList<ConsumableItem>();
-        this.waffen = new ArrayList<Waffe>();
-        waffen.add(w);
+		this.inventar = new ArrayList<ConsumableItem>();
+		this.waffen = new ArrayList<Waffe>();
+		waffen.add(w);
 
-        fov = new RayCaster();
-    }
+		fov = new RayCaster();
+	}
 
-    @Override
-    public void act() {
-        try {
-            char key;
-            key = frame.mainTerm().getKey();
-            switch (key) {
-                case 'q':
-                    switchWeapon(true);
-                    refreshStats();
-                    act();
-                    break;
-                case 'e':
-                    switchWeapon(false);
-                    refreshStats();
-                    act();
-                    break;
-                case 'f':
-                    if (fov instanceof RayCaster) {
-                        fov = new ViewEverything();
-                    } else {
-                        fov = new RayCaster();
-                    }
-                    refreshWorld();
-                    act();
-                    break;
-                case 'i':
-                    ConsumableItem it = chooseItem();
-                    if (it == null) {
-                        act();
-                    } else {
-                        consumeItem(it);
-                    }
-                    break;
-                case (char) 27:
-                    System.exit(0);
-                case 'g':
-                    godMode = !godMode;
-                    act();
-                    break;
-                case '\n':
-                    roundHouseKick();
-                    break;
-                default:
-                    Direction dir = Direction.keyToDir(key);
-                    if (dir != null) {
-                        tryToMove(dir);
-                    } else {
-                        act();
-                    }
-                    break;
-            }
-        } catch (InterruptedException e) {
-        }
-    }
+	public int getSemester() {
+		return semester;
+	}
 
-    public void switchWeapon(boolean backwards) {
-        if (backwards) {
-            Waffe tmp = waffen.remove(waffen.size() - 1);
-            waffen.add(0, tmp);
-        } else {
-            Waffe tmp = waffen.remove(0);
-            waffen.add(tmp);
-        }
-    }
+	@Override
+	public void act() {
+		try {
+			char key;
+			key = frame.mainTerm().getKey();
+			switch (key) {
+			case 'q':
+				switchWeapon(true);
+				refreshStats();
+				act();
+				break;
+			case 'e':
+				switchWeapon(false);
+				refreshStats();
+				act();
+				break;
+			case 'f':
+				if (fov instanceof RayCaster) {
+					fov = new ViewEverything();
+				} else {
+					fov = new RayCaster();
+				}
+				refreshWorld();
+				act();
+				break;
+			case 'i':
+				ConsumableItem it = chooseItem();
+				if (it == null) {
+					act();
+				} else {
+					consumeItem(it);
+				}
+				break;
+			case (char) 27:
+				System.exit(0);
+			case 'g':
+				godMode = !godMode;
+				act();
+				break;
+			case '\n':
+				roundHouseKick();
+				break;
+			default:
+				Direction dir = Direction.keyToDir(key);
+				if (dir != null) {
+					tryToMove(dir);
+				} else {
+					act();
+				}
+				break;
+			}
+		} catch (InterruptedException e) {
+		}
+	}
 
-    @Override
-    public Collection<Coordinate> getViewField() {
-        return fov.getViewField(world(), pos(), 100);
-    }
+	public void switchWeapon(boolean backwards) {
+		if (backwards) {
+			Waffe tmp = waffen.remove(waffen.size() - 1);
+			waffen.add(0, tmp);
+		} else {
+			Waffe tmp = waffen.remove(0);
+			waffen.add(tmp);
+		}
+	}
 
-    public void changeWorld(World world) {
-        world().removeActor(this);
-        world.addActor(this);
-    }
+	@Override
+	public Collection<Coordinate> getViewField() {
+		return fov.getViewField(world(), pos(), 100);
+	}
 
-    public void changeWorld(String level) {
-        changeWorld(Level.levelFromFile(level));
-    }
+	public void changeWorld(World world) {
+		world().removeActor(this);
+		world.addActor(this);
+		try {
+			((Level) world).fillWithEnemies();
+		} catch (TargetIsNotInThisWorldException e) {
+		}
+	}
 
-    public void refreshWorld() {
-        ((Level) world()).refresh(frame.mainTerm());
-    }
+	public void changeWorld(String level) {
+		changeWorld(Level.levelFromFile(level));
+	}
 
-    public void refreshStats() {
-        frame.bottomTerm().clearBuffer();
-        frame.bottomTerm().bufferString(0, 0, "Waffe: " + getActiveWeapon().getName()
-                + " (" + getActiveWeapon().getDamage() + ") "
-                + " | HP: " + healthPoints + "/" + maximalHealthPoints
-                + " | A: " + attackValue
-                + " | D: " + defenseValue
-                + " | I: " + intelligenceValue);
-        frame.bottomTerm().bufferString(0, 1, "Pi-Gebäude"
-                + " | $ " + money
-                + " | ECTS " + ects
-                + " | Sem " + semester);
-        frame.bottomTerm().bufferCameras();
-        frame.bottomTerm().refreshScreen();
-    }
+	public void refreshWorld() {
+		((Level) world()).refresh(frame.mainTerm());
+	}
 
-    public void toInventar(Item i) {
-        if (i instanceof Waffe) {
-            waffen.add((Waffe) i);
-        } else if (i instanceof ConsumableItem) {
-            inventar.add((ConsumableItem) i);
-        } else {
-            throw new IllegalStateException("Items should either be Weapons or consumable.");
-        }
-    }
+	public void refreshStats() {
+		frame.bottomTerm().clearBuffer();
+		frame.bottomTerm().bufferString(
+				0,
+				0,
+				"Waffe: " + getActiveWeapon().getName() + " ("
+						+ getActiveWeapon().getDamage() + ") " + " | HP: "
+						+ healthPoints + "/" + maximalHealthPoints + " | A: "
+						+ attackValue + " | D: " + defenseValue + " | I: "
+						+ intelligenceValue);
+		frame.bottomTerm().bufferString(
+				0,
+				1,
+				"Pi-Gebäude" + " | € " + money + " | ECTS " + ects + " | Sem "
+						+ semester);
+		frame.bottomTerm().bufferCameras();
+		frame.bottomTerm().refreshScreen();
+	}
 
-    @Override
-    public Waffe getActiveWeapon() {
-        return waffen.get(0);
-    }
+	public void toInventar(Item i) {
+		if (i instanceof Waffe) {
+			waffen.add((Waffe) i);
+		} else if (i instanceof ConsumableItem) {
+			inventar.add((ConsumableItem) i);
+		} else {
+			throw new IllegalStateException(
+					"Items should either be Weapons or consumable.");
+		}
+	}
 
-    private ConsumableItem chooseItem() {
-        ConsumableItem output = null;
-        if (inventar.isEmpty()) {
-            ZombieTools.sendMessage("Inventar ist leer.", frame);
-            return null;
-        }
-        frame.mainTerm().clearBuffer();
-        frame.mainTerm().bufferString(0, 0, "Inventarliste:");
-        for (int i = 0; i < inventar.size(); i++) {
-            Item it = inventar.get(i);
-            frame.mainTerm().bufferString(0, 2 + i, "[" + ((char) (97 + i)) + "] " + it.face() + " - " + it.getName());
-        }
-        frame.mainTerm().refreshScreen();
-        try {
-            int key = ((int) frame.mainTerm().getKey()) - 97;
-            if (key >= 0 && key <= 25 && key < inventar.size()) {
-                output = inventar.get(key);
-            }
-        } catch (InterruptedException ex) {
-        }
-        refreshWorld();
-        return output;
-    }
+	@Override
+	public Waffe getActiveWeapon() {
+		return waffen.get(0);
+	}
 
-    public void heal(int i) {
-        System.out.print(getName() + " hat " + i + " HP geheilt. ");
-        healthPoints += i;
-        if (healthPoints >= maximalHealthPoints) {
-            healthPoints = maximalHealthPoints;
-        }
-        System.out.println("HP: " + healthPoints);
-    }
+	private ConsumableItem chooseItem() {
+		ConsumableItem output = null;
+		if (inventar.isEmpty()) {
+			ZombieTools.sendMessage("Inventar ist leer.", frame);
+			return null;
+		}
+		frame.mainTerm().clearBuffer();
+		frame.mainTerm().bufferString(0, 0, "Inventarliste:");
+		for (int i = 0; i < inventar.size(); i++) {
+			Item it = inventar.get(i);
+			frame.mainTerm().bufferString(
+					0,
+					2 + i,
+					"[" + ((char) (97 + i)) + "] " + it.face() + " - "
+							+ it.getName());
+		}
+		frame.mainTerm().refreshScreen();
+		try {
+			int key = ((int) frame.mainTerm().getKey()) - 97;
+			if (key >= 0 && key <= 25 && key < inventar.size()) {
+				output = inventar.get(key);
+			}
+		} catch (InterruptedException ex) {
+		}
+		refreshWorld();
+		return output;
+	}
 
-    private void consumeItem(ConsumableItem it) {
-        ZombieTools.sendMessage("Du benutzt '" + it.getName() + "'.", frame);
-        System.out.println(getName() + " benutzt Item " + it.getName());
-        it.getConsumedBy(this);
-        inventar.remove(it);
-        it.expire();
-        refreshStats();
-    }
+	public void heal(int i) {
+		System.out.print(getName() + " hat " + i + " HP geheilt. ");
+		healthPoints += i;
+		if (healthPoints >= maximalHealthPoints) {
+			healthPoints = maximalHealthPoints;
+		}
+		System.out.println("HP: " + healthPoints);
+	}
 
-    @Override
-    protected void killed(Creature killer) {
-        // TODO: Im Endscreen dynamisch Informationen anzeigen.
-        try {
-            Screen.showImage(frame.mainTerm(), "src/sources/endscreen.txt");
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Player.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        System.exit(0);
-    }
+	private void consumeItem(ConsumableItem it) {
+		ZombieTools.sendMessage("Du benutzt '" + it.getName() + "'.", frame);
+		System.out.println(getName() + " benutzt Item " + it.getName());
+		it.getConsumedBy(this);
+		inventar.remove(it);
+		it.expire();
+		refreshStats();
+	}
 
-    public void addMoney(int m) {
-        this.money += m;
-    }
+	@Override
+	protected void killed(Creature killer) {
+		// TODO: Im Endscreen dynamisch Informationen anzeigen.
+		try {
+			Screen.showImage(frame.mainTerm(), "src/sources/endscreen.txt");
+		} catch (InterruptedException ex) {
+			Logger.getLogger(Player.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		}
+		System.exit(0);
+	}
+
+	public void addMoney(int m) {
+		this.money += m;
+	}
 }
--- src/zombiefu/util/ZombieTools.java
+++ src/zombiefu/util/ZombieTools.java
@@ -23,93 +23,99 @@ import zombiefu.ui.ZombieFrame;
 
 public class ZombieTools {
 
-    public static Player activePlayer;
+	public static Player activePlayer;
 
-    public static void createStoryForPlayer(Player player) {
-        Level world = createWorld();
-        world.addActor(player);
-    }
+	public static void createStoryForPlayer(Player player) {
+		Level world = createWorld();
+		world.addActor(player);
+		try {
+			world.fillWithEnemies();
+		} catch (TargetIsNotInThisWorldException e) {
+		}
+	}
 
-    public static void createBidirectionalTeleporter(World world1,
-            Coordinate from1, Coordinate to2, World world2, Coordinate from2,
-            Coordinate to1) {
-        /*
-         * fromi: Wo befindet sich der Teleporter in Welt i? toi: Wo soll der
-         * Player in Welt i hinteleportiert werden?
-         */
-        Teleporter tel1 = new Teleporter(world2, to2);
-        Teleporter tel2 = new Teleporter(world1, to1);
-        world1.addActor(tel1, from1);
-        world2.addActor(tel2, from2);
-    }
+	public static void createBidirectionalTeleporter(World world1,
+			Coordinate from1, Coordinate to2, World world2, Coordinate from2,
+			Coordinate to1) {
+		/*
+		 * fromi: Wo befindet sich der Teleporter in Welt i? toi: Wo soll der
+		 * Player in Welt i hinteleportiert werden?
+		 */
+		Teleporter tel1 = new Teleporter(world2, to2);
+		Teleporter tel2 = new Teleporter(world1, to1);
+		world1.addActor(tel1, from1);
+		world2.addActor(tel2, from2);
+	}
 
-    private static Level createWorld() {
-        String srcs = "src/sources/";
-        String[] levels = Screen.getStrings(srcs + "levels.txt");
-        String[] teles = Screen.getStrings(srcs + "teleporters.txt");
-        HashMap<String, Level> nameOfLevels = new HashMap<String, Level>();
-        for (String s : levels) {
-            nameOfLevels.put(s, Level.levelFromFile(srcs + s + ".txt"));
-        }
-        for (String s : teles) {
-            try {
-                String[] d = s.split(" ");
-                Level world1 = nameOfLevels.get(d[0]);
-                Coordinate from1 = new Coordinate(Integer.decode(d[1]),
-                        Integer.decode(d[2]));
-                Coordinate to2 = new Coordinate(Integer.decode(d[3]),
-                        Integer.decode(d[4]));
-                Level world2 = nameOfLevels.get(d[5]);
-                Coordinate from2 = new Coordinate(Integer.decode(d[6]),
-                        Integer.decode(d[7]));
-                Coordinate to1 = new Coordinate(Integer.decode(d[8]),
-                        Integer.decode(d[9]));
-                createBidirectionalTeleporter(world1, from1, to2, world2,
-                        from2, to1);
-            } catch (Exception e) {
-            }
-        }
-        return nameOfLevels.get(levels[0]);
-    }
+	private static Level createWorld() {
+		String srcs = "src/sources/";
+		String[] levels = Screen.getStrings(srcs + "levels.txt");
+		String[] teles = Screen.getStrings(srcs + "teleporters.txt");
+		HashMap<String, Level> nameOfLevels = new HashMap<String, Level>();
+		for (String s : levels) {
+			nameOfLevels.put(s, Level.levelFromFile(srcs + s + ".txt"));
+		}
+		for (String s : teles) {
+			try {
+				String[] d = s.split(" ");
+				Level world1 = nameOfLevels.get(d[0]);
+				Coordinate from1 = new Coordinate(Integer.decode(d[1]),
+						Integer.decode(d[2]));
+				Coordinate to2 = new Coordinate(Integer.decode(d[3]),
+						Integer.decode(d[4]));
+				Level world2 = nameOfLevels.get(d[5]);
+				Coordinate from2 = new Coordinate(Integer.decode(d[6]),
+						Integer.decode(d[7]));
+				Coordinate to1 = new Coordinate(Integer.decode(d[8]),
+						Integer.decode(d[9]));
+				createBidirectionalTeleporter(world1, from1, to2, world2,
+						from2, to1);
+			} catch (Exception e) {
+			}
+		}
+		return nameOfLevels.get(levels[0]);
+	}
 
-    public static List<Direction> getAllowedDirections() {
-        return Arrays.asList(Direction.SOUTH, Direction.EAST, Direction.WEST, Direction.NORTH);
-    }
+	public static List<Direction> getAllowedDirections() {
+		return Arrays.asList(Direction.SOUTH, Direction.EAST, Direction.WEST,
+				Direction.NORTH);
+	}
 
-    public static Direction getRandomDirection() {
-        return Dice.global.choose(getAllowedDirections());
-    }
+	public static Direction getRandomDirection() {
+		return Dice.global.choose(getAllowedDirections());
+	}
 
-    public static void sendMessage(String s, ZombieFrame frame) {
-        activePlayer.refreshWorld();
-        frame.topTerm().clearBuffer();
-        frame.topTerm().bufferString(0, 0, s);
-        frame.topTerm().refreshScreen();
-        char key = 0;
-        try {
-            while (key != '\n') {
-                key = frame.mainTerm().getKey();
-            }
-        } catch (InterruptedException ex) {
-        }
-        frame.topTerm().clearBuffer();
-        frame.topTerm().refreshScreen();
-    }
+	public static void sendMessage(String s, ZombieFrame frame) {
+		activePlayer.refreshWorld();
+		frame.topTerm().clearBuffer();
+		frame.topTerm().bufferString(0, 0, s);
+		frame.topTerm().refreshScreen();
+		char key = 0;
+		try {
+			while (key != '\n') {
+				key = frame.mainTerm().getKey();
+			}
+		} catch (InterruptedException ex) {
+		}
+		frame.topTerm().clearBuffer();
+		frame.topTerm().refreshScreen();
+	}
 
-    public static void sendMessage(String string) {
-        Guard.argumentIsNotNull(activePlayer);
-        sendMessage(string, activePlayer.frame);
-    }
+	public static void sendMessage(String string) {
+		Guard.argumentIsNotNull(activePlayer);
+		sendMessage(string, activePlayer.frame);
+	}
 
-    public static void registerPlayer(Player pl) {
-        activePlayer = pl;
-    }
-    
-    public static Color getRandomColor(Dice d) {
-        return new Color(d.nextInt(0, 255),d.nextInt(0, 255),d.nextInt(0, 255));
-    }
-    
-    public static Color getRandomColor() {
-        return getRandomColor(Dice.global);
-    }
+	public static void registerPlayer(Player pl) {
+		activePlayer = pl;
+	}
+
+	public static Color getRandomColor(Dice d) {
+		return new Color(d.nextInt(0, 255), d.nextInt(0, 255),
+				d.nextInt(0, 255));
+	}
+
+	public static Color getRandomColor() {
+		return getRandomColor(Dice.global);
+	}
 }
