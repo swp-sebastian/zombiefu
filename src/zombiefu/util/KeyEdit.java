a12bb89a07152df464a734f46fad4189bc9fe92f
Sebastian <sgs@zedat.fu-berlin.de>
2013-03-13 17:43:46 +0100

    Letzter Commit unvollständig.

--- src/zombiefu/util/KeyEdit.java
+++ src/zombiefu/util/KeyEdit.java
@@ -1,36 +1,53 @@
 package zombiefu.util;
 
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import zombiefu.util.Action;
+
 public class KeyEdit {
-	private static String name;
-	public static char up;
-	public static char down;
-	public static char left;
-	public static char right;
-	public static char hit;
-	public static char nextItem;
-	public static char prevItem;
-	public static char showItems;
-	
-	public static String getPlayername(){
-		if (name.equalsIgnoreCase("playername")){ //Wenn die Konfigurationsdatei nicht geändert wurde,
-			name = System.getProperty("user.name"); // wird der Benutzername als Spielername gesetzt
-		}
-		return name;
-	}
-	
-	public static void read(String[] config){
-		for (int i =0;i<config.length;i++){
-			String[] splitResult = config[i].split("=");
-			config[i]= splitResult[1];
-		}
-		name = config[0];
-		up = config[1].charAt(0);
-		down = config[2].charAt(0);
-		left = config[3].charAt(0);
-		right = config[4].charAt(0);
-		hit = config[5].charAt(0);
-		nextItem = config[6].charAt(0);
-		prevItem = config[7].charAt(0);
-		showItems = config[8].charAt(0);
-	}
+    private static Properties props;
+    private static String name;
+    private static HashMap<Character, Action> keys;
+
+    public static String getPlayername(){
+        return name;
+    }
+
+    public static Action getAction(char c) {
+        // Keine Ahnung wie wir sonst <Enter> erkennen.
+        if (Character.getNumericValue(c) == -1) {
+            // Und das wäre <SPC>, wir binden das zu '_'
+            if (c == ' ') {
+                return keys.get(new Character('_'));
+            }
+            return keys.get(new Character(' '));
+        }
+
+        return keys.get(Character.valueOf(c));
+    }
+
+    public static void read(String config){
+        props = new Properties();
+        try {
+            props.load(new FileInputStream(config));
+            System.out.println("Konfigurationsdatei "+config+" geladen.");
+        } catch (IOException ex) {
+            System.out.println("Konfigurationsdatei "+config+" nicht vorhanden.");
+        }
+
+        name = props.getProperty("name", System.getProperty("user.name"));
+
+        keys = new HashMap<Character, Action>();
+
+        keys.put(Character.valueOf(props.getProperty("controls.up", "w").charAt(0)), Action.UP);
+        keys.put(Character.valueOf(props.getProperty("controls.down", "s").charAt(0)), Action.DOWN);
+        keys.put(Character.valueOf(props.getProperty("controls.left", "a").charAt(0)), Action.LEFT);
+        keys.put(Character.valueOf(props.getProperty("controls.right", "d").charAt(0)), Action.RIGHT);
+        keys.put(Character.valueOf(props.getProperty("controls.attack", Character.toString(' ')).charAt(0)), Action.ATTACK);
+        keys.put(Character.valueOf(props.getProperty("controls.nextweapon", "e").charAt(0)), Action.NEXT_WEAPON);
+        keys.put(Character.valueOf(props.getProperty("controls.prevweapon", "q").charAt(0)), Action.PREV_WEAPON);
+        keys.put(Character.valueOf(props.getProperty("controls.inventory", "i").charAt(0)), Action.INVENTORY);
+    }
 }
