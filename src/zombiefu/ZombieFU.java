878d141d48f48a172682cc10dd38a0dd4f05a6d9
adrianhe <adrian.henning@fu-berlin.de>
2013-03-14 18:01:21 +0100

    auswahl des studiengangs über screen

--- src/zombiefu/ZombieFU.java
+++ src/zombiefu/ZombieFU.java
@@ -7,10 +7,9 @@ public class ZombieFU {
     public static void main(String[] args) {
 
         ZombieGame.createGame(args, "The Final Exam - Die Anwesenheitspflicht schlägt zurück");
+        ZombieGame.showStaticImage("startscreen", true);
+        ZombieGame.showStaticImage("story", true);
         ZombieGame.initialize();
-
-        ZombieGame.showStaticImage("startscreen");
-        ZombieGame.showStaticImage("story");
 
         ZombieGame.startGame();
 
