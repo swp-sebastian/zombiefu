3ef00d0e548779102f682dca9954b013ea327bbf
Sebastian <sgs@zedat.fu-berlin.de>
2013-03-14 18:47:52 +0100

    showStaticImage() Misere besser gelöst.

--- src/zombiefu/ZombieFU.java
+++ src/zombiefu/ZombieFU.java
@@ -7,8 +7,8 @@ public class ZombieFU {
     public static void main(String[] args) {
 
         ZombieGame.createGame(args, "The Final Exam - Die Anwesenheitspflicht schlägt zurück");
-        ZombieGame.showStaticImage("startscreen", true);
-        ZombieGame.showStaticImage("story", true);
+        ZombieGame.showStaticImage("startscreen");
+        ZombieGame.showStaticImage("story");
         ZombieGame.initialize();
 
         ZombieGame.startGame();
