ce439038d3a0021eb25b59c4ed0890f0c8b96c88
Walfischfutter <tomas.bayer@gmail.com>
2013-03-14 11:55:57 +0100

    fix last commit

---
+++ src/zombiefu/util/Discipline.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombiefu.util;
+
+/**
+ *
+ * @author tomas
+ */
+public enum Discipline {
+    
+    POLITICAL_SCIENCE,
+    COMPUTER_SCIENCE,
+    MEDICINE,
+    PHILOSOPHY,
+    PHYSICS,
+    BUSINESS,
+    CHEMISTRY,
+    SPORTS,
+    MATHEMATICS;
+    
+}
